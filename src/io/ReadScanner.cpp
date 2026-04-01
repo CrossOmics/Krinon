0407b912650e3f0f2d773a732e61ff797b661ad8
Yuxuan Zhang <1002638696@qq.com>
2026-04-01 12:24:19 +0800

    try to fix a bug in parseRead

--- src/io/ReadScanner.cpp
+++ src/io/ReadScanner.cpp
@@ -154,6 +154,7 @@ namespace RefactorProcessing{
     // }
     size_t ReadScanner::parseRead(Read &r, const char* readBuffer1, const char* readBuffer2) {
         const char* ptr1 = readBuffer1;
+        if (*ptr1 == 0) return 0; // invalid input
         int len = std::strcspn(ptr1, " ");
         if (len == 0) return 0; // invalid read ("\000\000\000\000"), is it expected to happen?
         r.name = std::string(ptr1 , len ); // skip '@'
